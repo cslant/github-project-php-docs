e7623b462ff2c35d2598afe5d29c936ee9296ffb
thuankg1752 <thuankg1752@gmail.com>
2025-02-16 20:38:08 +0700

    feat: update sidebar, remove md features

--- sidebar.ts
+++ sidebar.ts
@@ -5,5 +5,95 @@ module.exports = {
       id: 'README',
       label: '👋 README',
     },
+    {
+      type: 'doc',
+      id: 'introduction',
+      label: 'Introduction',
+    },
+    {
+      type: 'category',
+      link: {
+        type: 'generated-index',
+        title: 'Prologue - Github Project PHP Docs',
+        description: 'This section will give you a brief overview of the project and its features. Let\'s get started with the Github Project PHP Docs.',
+      },
+      label: 'Prologue',
+      items: [
+        {
+          type: 'doc',
+          id: 'prologue/releases',
+          label: '🚀 Release Notes',
+        },
+      ],
+      collapsed: false,
+    },
+    {
+      type: 'category',
+      link: {
+        type: 'generated-index',
+        title: 'Getting Started - Github Project PHP Docs',
+        description: 'Let\'s get started with the Github Project PHP Docs.' +
+          'You can also find the installation guide and the configuration guide here.',
+      },
+      label: 'Getting Started',
+      items: [
+        {
+          type: 'doc',
+          id: 'getting-started/requirements',
+          label: '📋 Requirements',
+        },
+        {
+          type: 'doc',
+          id: 'getting-started/installation',
+          label: '🔧 Installation',
+        },
+        {
+          type: 'doc',
+          id: 'getting-started/configuration',
+          label: '🛠 Configuration',
+        },
+      ],
+      collapsed: false,
+    },
+    {
+      type: 'category',
+      link: {
+        title: 'Support -Github Project PHP Docs',
+        type: 'generated-index',
+        description: 'Please check this section if you have any problems with the bot or you want to request a new feature. We will be happy to help you!',
+      },
+      label: 'Support',
+      items: [
+        {
+          type: 'doc',
+          id: 'support/feature-requests',
+          label: '🚀 Feature Requests',
+        },
+        {
+          type: 'doc',
+          id: 'support/issues',
+          label: '🐛 Issues',
+        },
+      ],
+      collapsed: false,
+    },
+    {
+      type: 'category',
+      link: {
+        type: 'generated-index',
+        title: 'GitHub Source - Github Project PHP Docs',
+        description: 'Please check this section if you want to contribute to the Github Project PHP Docs project. We will be happy to accept your contributions. Let\'s make the package better together!',
+      },
+      label: 'GitHub Source',
+      collapsed: false,
+      items: [
+        {
+          type: 'link',
+          label: 'Github Project PHP Docs',
+          description: 'Check the source code of Github Project PHP Docs on GitHub.',
+          href: 'https://github.com/cslant/github-project-php-docs',
+        },
+      ],
+    },
   ],
 };
